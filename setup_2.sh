b33fbbecb50f050c86737d489f899c38b5b6e11e
ehafidi <53328741+ehafidi@users.noreply.github.com>
2021-01-17 20:48:34 +0100

    Add files via upload

---
+++ setup_2.sh
@@ -0,0 +1,3 @@
+#brew install minikube # minikube installe automatiquement kubectl et docker
+export MINIKUBE_HOME=/Users/ehafidi/goinfre 
+minikube start --vm-driver=virtualbox #pas utiliser hyperkit
