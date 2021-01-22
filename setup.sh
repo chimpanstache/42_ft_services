c30a8b8a62ce733916c7593d582a7b5ce2ab1c57
ehafidi <53328741+ehafidi@users.noreply.github.com>
2021-01-22 20:42:20 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,6 +1,31 @@
-minikube version
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install docker
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install docker-machine
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install kubectl
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install minikube
+
+# export MINIKUBE_HOME=/Users/ehafidi/goinfre 
+
 minikube start --vm-driver=virtualbox
-kubectl version
-kubectl cluster-info
-kubectl get nodes
-kubectl proxy
+#minikube status
+eval $(minikube docker-env)
+
+minikube addons enable dashboard
+minikube addons enable metallb
+
+minikube dashboard & #go to k8s dashboard on web browser
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f metallb/config.yaml # and not create !!!!!!!!!!
+
+
+
+
+docker build -t nginx-container srcs/nginx
+kubectl create -f srcs/nginx/nginx.yaml #create a pod using the data in nginx.yaml
+# docker run -it -p 80:80 -p 443:443 --name nginx-container nginx-container #no need!!!!!!!!
+
+
+#kubectl delete --all deployment
+#kubectl delete --all svc
